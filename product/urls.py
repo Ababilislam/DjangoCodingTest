2aa3f67239311f9acfb9969d8f1444fb354eac2a
ABABIL ISLAM UDOY <ababilislamudoy@gmail.com>
2023-04-27 16:22:48 +0600

    node js connected

---
+++ product/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+from product.views import *
+app_name = "product"
+urlpatterns = [
+    # Variants URLs
+    path('variants/', VariantView.as_view(), name='variants'),
+    path('variant/create', VariantCreateView.as_view(), name='create.variant'),
+    path('variant/<int:id>/edit', VariantEditView.as_view(), name='update.variant'),
+    #
+    # Products URLs
+    path('create/', CreateProductView.as_view(), name='create.product'),
+    path('list/', TemplateView.as_view(template_name='product/list.html', extra_context={
+        'product': True
+    }), name='list.product'),
+]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+from product.models import Variant
+from django.views.generic import ListView, CreateView, UpdateView
+
+from product.forms import VariantForm
+
+
+class BaseVariantView(generic.View):
+    form_class = VariantForm
+    model = Variant
+    template_name = '/product/create.html'
+    success_url = '/product/variants'
+
+
+class VariantView(BaseVariantView, ListView):
+    template_name = 'product/list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        filter_string = {}
+        print(self.request.GET)
+        for key in self.request.GET:
+            if self.request.GET.get(key):
+                filter_string[key] = self.request.GET.get(key)
+        return Variant.objects.filter(**filter_string)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = True
+        context['request'] = ''
+        if self.request.GET:
+            context['request'] = self.request.GET['title__icontains']
+        return context
+
+
+class VariantCreateView(BaseVariantView, CreateView):
+    pass
+
+
+class VariantEditView(BaseVariantView, UpdateView):
+    pk_url_kwarg = 'id'
+
+
+
+class CreateProductView(generic.TemplateView):
+    template_name = 'product/create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateProductView, self).get_context_data(**kwargs)
+        variants = Variant.objects.filter(active=True).values('id', 'title')
+        context['product'] = True
+        context['variants'] = list(variants.all())
+        return context
